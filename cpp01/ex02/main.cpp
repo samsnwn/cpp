42351febb8c13b168c2524589e3e5cedd8aa6113
samsnwn <samuel.c.glantz@gmail.com>
2025-11-26 13:56:31 +0100

    style: Reformat main function and remove comments and explicit return.

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include <string>
 
-int main(void)
-{
-	std::string string = "HI THIS IS BRAIN";
-	std::string *stringPTR = &string; // stringPTR: a pointer to the string.
-	std::string &stringREF = string; // stringREF: a reference to the string.
+int main(void) {
+  std::string string = "HI THIS IS BRAIN";
+  std::string *stringPTR = &string;
+  std::string &stringREF = string;
 
-	std::cout << &string << std::endl; // The memory address of the string variable.
-	std::cout << stringPTR << std::endl; // The memory address held by stringPTR.
-	std::cout << &stringREF << std::endl; // The memory address held by stringREF.
+  std::cout << &string << std::endl;
+  std::cout << stringPTR << std::endl;
+  std::cout << &stringREF << std::endl;
 
-	std::cout << string << std::endl; // The value of the string variable.
-	std::cout << *stringPTR << std::endl; // The value pointed to by stringPTR.
-	std::cout << stringREF << std::endl; // The value pointed to by stringREF.
+  std::cout << string << std::endl;
+  std::cout << *stringPTR << std::endl;
+  std::cout << stringREF << std::endl;
 }
